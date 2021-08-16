49a278c4cc7e673a539012c9ee9cb3228644d917
monad-systems <88200628+monad-systems@users.noreply.github.com>
2021-08-16 09:32:03 +0000

    Deploying to gh-pages from @ monad-systems/monad.systems@5ab9abbe95f9b219da65188b3cbcecb242fb0723 🚀

---
+++ _next/static/OaPUjVa3Mng5Az7a-1Z7I/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/551-b14b0c157ffb3e2abe35.js","static/chunks/pages/index-8dbc499f11bb5ef319f0.js"],"/_error":["static/chunks/pages/_error-9faf4177fb4e528b4124.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
