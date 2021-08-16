6b9ec914f47d99d36cafdbf5432c25ef349eb37d
monad-systems <88200628+monad-systems@users.noreply.github.com>
2021-08-16 13:52:11 +0000

    Deploying to gh-pages from @ monad-systems/monad.systems@9a6e71c5ebdce4af41d4dcfe0635ac23bcc4f846 🚀

---
+++ _next/static/GbC4gEVdP9qwdxugs-HNw/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/551-b14b0c157ffb3e2abe35.js","static/chunks/pages/index-b8bfce922a1d9b1ad9b4.js"],"/_error":["static/chunks/pages/_error-9faf4177fb4e528b4124.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/eYwxa0aM5_1CFi-j0zYtv/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/551-b14b0c157ffb3e2abe35.js","static/chunks/pages/index-e734c1d2e9054a6a18f3.js"],"/_error":["static/chunks/pages/_error-9faf4177fb4e528b4124.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
