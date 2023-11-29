05d4a9f303d632831164337a0aabd12d078b1035
Ferenc Sticza <ferenc.sticza@gmail.com>
2023-11-29 13:22:29 +0100

    fix: next config update

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 module.exports = {
+  output: 'export',
   reactStrictMode: true,
   images: {
     loader: 'cloudinary',
     path: 'https://res.cloudinary.com/monad-systems/image/upload/',
   },
-}
+};
