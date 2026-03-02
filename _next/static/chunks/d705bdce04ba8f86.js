f4e1d3b68e23913cef93506bee79bdc38fde511e
monad-systems <88200628+monad-systems@users.noreply.github.com>
2026-03-02 09:25:20 +0000

    Deploying to gh-pages from @ monad-systems/monad.hu@0c0f7d8d02acddd308ef3b4c355265904b9846e3 🚀

--- _next/static/chunks/d705bdce04ba8f86.js
+++ _next/static/chunks/d705bdce04ba8f86.js
@@ -2,6 +2,6 @@ __turbopack_load_page_chunks__("/_app", [
   "static/chunks/d7caf79a03fcc709.js",
   "static/chunks/9ac8acb171aa0226.js",
   "static/chunks/5e48a92fce96495c.js",
-  "static/chunks/a781b7eaa18c0bb8.css",
-  "static/chunks/turbopack-543326446c6a57bb.js"
+  "static/chunks/6d0cc2c1a23a2594.css",
+  "static/chunks/turbopack-a46cded56d5db21f.js"
 ])
