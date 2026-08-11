19a676cc795c42f9f23d4825f37d3dc80727f43a
fsticza <fsticza@users.noreply.github.com>
2026-08-11 11:09:15 +0000

    Deploying to gh-pages from @ monad-systems/monad.hu@15437b3e9142ae08a8c99a444a6935706606b625 🚀

--- _next/static/chunks/6d167ab7ec8af341.js
+++ _next/static/chunks/6d167ab7ec8af341.js
@@ -2,6 +2,6 @@ __turbopack_load_page_chunks__("/_app", [
   "static/chunks/619fd99c753ba0e1.js",
   "static/chunks/8b8488c8797dbe88.js",
   "static/chunks/4b26e7b9e209e280.js",
-  "static/chunks/6d7cb26f91a20923.css",
-  "static/chunks/turbopack-3a47d8e79351e049.js"
+  "static/chunks/7a2ac13479287277.css",
+  "static/chunks/turbopack-e9c4256c9dafb03f.js"
 ])
